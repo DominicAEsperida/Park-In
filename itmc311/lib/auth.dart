2044c0707be15d94b8e4de3f12b9032366ff7b5d
mathew <90034902+mathewwww@users.noreply.github.com>
2023-12-11 01:30:13 +0800

    Pages: Added auth.dart

---
+++ itmc311/lib/auth.dart
@@ -0,0 +1,11 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+class Authentication {
+  FirebaseAuth _auth = FirebaseAuth.instance;
+  Future <User?> signInWithEmailAndPassword(String email, String password) async{
+    
+      UserCredential credential =await _auth.signInWithEmailAndPassword(email: email, password: password);
+      return credential.user;
+  }
+
+}
