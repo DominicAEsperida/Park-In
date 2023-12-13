deba3eb2300389dc8ab9d090df324d86462f5665
MNJDen <kajoo.alden20@gmail.com>
2023-12-13 20:27:54 +0800

    Pages: Update Log In Page

--- itmc311/lib/Pages/LogInPageAdmin.dart
+++ itmc311/lib/Pages/LogInPageAdmin.dart
@@ -5,7 +5,6 @@ import 'package:itmc311/Pages/LandingPage.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:itmc311/auth.dart';
 
-
 class LogInPageAdmin extends StatefulWidget {
   const LogInPageAdmin({super.key});
 
@@ -14,21 +13,17 @@ class LogInPageAdmin extends StatefulWidget {
 }
 
 class _LogInPageAdminState extends State<LogInPageAdmin> {
-
   final Authentication _auth = Authentication();
 
   TextEditingController _emailController = TextEditingController();
   TextEditingController _passwordController = TextEditingController();
 
-
   @override
-  
-void dispose() {
-  _emailController.dispose();
-  _passwordController.dispose();
-  super.dispose();
-}
-
+  void dispose() {
+    _emailController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
 
   Widget build(BuildContext context) {
     return Scaffold(
@@ -53,174 +48,199 @@ void dispose() {
           ),
         ),
       ),
-      body: Container(
-        padding:  EdgeInsets.fromLTRB(20.0, 0.0, 20.0, 0.0),
-        child: Center(
-          child: Column(
-            children: [
-              const SizedBox(
-                height: 30.0,
-              ),
-              Image.asset(
-                'assets/admin.png',
-                height: 150.0,
-                width: 150.0,
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 20),
-                child: const Text(
-                  'Log In Your Account',
-                  style: TextStyle(
-                    fontFamily: 'BubbleboddyNeue',
-                    fontSize: 32.0,
-                    fontWeight: FontWeight.bold,
-                    color: Color.fromRGBO(10, 10, 31, 1),
+      body: SingleChildScrollView(
+        physics: BouncingScrollPhysics(),
+        child: Container(
+          padding: EdgeInsets.fromLTRB(20.0, 0.0, 20.0, 0.0),
+          child: Center(
+            child: Column(
+              children: [
+                const SizedBox(
+                  height: 30.0,
+                ),
+                Image.asset(
+                  'assets/admin.png',
+                  height: 150.0,
+                  width: 150.0,
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 20),
+                  child: const Text(
+                    'Log In Your Account',
+                    style: TextStyle(
+                      fontFamily: 'BubbleboddyNeue',
+                      fontSize: 32.0,
+                      fontWeight: FontWeight.bold,
+                      color: Color.fromRGBO(10, 10, 31, 1),
+                    ),
                   ),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 10),
-                child: const Text(
-                  'Welcome back! Enter your admin details.',
-                  style: TextStyle(
-                    fontFamily: 'BubbleboddyNeue',
-                    fontSize: 16.0,
-                    fontWeight: FontWeight.w300,
-                    color: Color.fromRGBO(10, 10, 31, 1),
+                Padding(
+                  padding: const EdgeInsets.only(top: 10),
+                  child: const Text(
+                    'Welcome back! Enter your admin details.',
+                    style: TextStyle(
+                      fontFamily: 'BubbleboddyNeue',
+                      fontSize: 16.0,
+                      fontWeight: FontWeight.w300,
+                      color: Color.fromRGBO(10, 10, 31, 1),
+                    ),
                   ),
                 ),
-              ),
-              Padding(
-                //TextFormField
-                padding: const EdgeInsets.fromLTRB(0.0, 40.0, 0.0, 20.0),
-                child: TextFormField(
-                  controller: _emailController,
-                  decoration: InputDecoration(
-                    helperText: "Ex: Admin@gmail.com",
-                    filled: true,
-                    fillColor: Colors.white,
-                    labelText: 'Email',
-                    labelStyle: const TextStyle(
-                      // color: Color.fromRGBO(0, 0, 255, 1),
-                      fontSize: 15.0,
+                Padding(
+                  //TextFormField
+                  padding: const EdgeInsets.fromLTRB(0.0, 40.0, 0.0, 20.0),
+                  child: TextFormField(
+                    style: TextStyle(
                       fontFamily: 'Inter',
                     ),
-                    enabledBorder: OutlineInputBorder(
-                      borderSide: const BorderSide(
-                        width: 1,
-                        color: Color.fromRGBO(0, 0, 255, 1),
+                    controller: _emailController,
+                    decoration: InputDecoration(
+                      helperText: "Ex: Admin@gmail.com",
+                      filled: true,
+                      fillColor: Colors.white,
+                      labelText: 'Email',
+                      labelStyle: const TextStyle(
+                        // color: Color.fromRGBO(0, 0, 255, 1),
+                        fontSize: 15.0,
+                        fontFamily: 'Inter',
                       ),
-                      borderRadius: BorderRadius.circular(15.0),
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                      borderSide: const BorderSide(
-                        width: 1,
-                        color: Color.fromRGBO(0, 0, 255, 1),
+                      helperStyle: TextStyle(
+                        fontFamily: 'Inter',
                       ),
-                      borderRadius: BorderRadius.circular(15.0),
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: const BorderSide(
+                          width: 1,
+                          color: Color.fromRGBO(0, 0, 255, 1),
+                        ),
+                        borderRadius: BorderRadius.circular(15.0),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: const BorderSide(
+                          width: 1,
+                          color: Color.fromRGBO(0, 0, 255, 1),
+                        ),
+                        borderRadius: BorderRadius.circular(15.0),
+                      ),
                     ),
                   ),
                 ),
-              ),
-              Padding(
-                //TextFormField
-                padding: const EdgeInsets.fromLTRB(0.0, 0.0, 0.0, 50.0),
-                child: TextFormField(
-                  controller: _passwordController,
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    filled: true,
-                    fillColor: Colors.white,
-                    labelText: 'Password',
-                    labelStyle: const TextStyle(
-                      // color: Color.fromRGBO(0, 0, 255, 1),
-                      fontSize: 15.0,
+                Padding(
+                  //TextFormField
+                  padding: const EdgeInsets.fromLTRB(0.0, 0.0, 0.0, 50.0),
+                  child: TextFormField(
+                    style: TextStyle(
                       fontFamily: 'Inter',
                     ),
-                    enabledBorder: OutlineInputBorder(
-                      borderSide: const BorderSide(
-                        width: 1,
-                        color: Color.fromRGBO(0, 0, 255, 1),
+                    controller: _passwordController,
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      filled: true,
+                      fillColor: Colors.white,
+                      labelText: 'Password',
+                      labelStyle: const TextStyle(
+                        // color: Color.fromRGBO(0, 0, 255, 1),
+                        fontSize: 15.0,
+                        fontFamily: 'Inter',
                       ),
-                      borderRadius: BorderRadius.circular(15.0),
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                      borderSide: const BorderSide(
-                        width: 1,
-                        color: Color.fromRGBO(0, 0, 255, 1),
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: const BorderSide(
+                          width: 1,
+                          color: Color.fromRGBO(0, 0, 255, 1),
+                        ),
+                        borderRadius: BorderRadius.circular(15.0),
                       ),
-                      borderRadius: BorderRadius.circular(15.0),
-                    ),
-                  ),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(10.0),
-                child: ElevatedButton(
-                  onPressed: _Login,
-                  style: ElevatedButton.styleFrom(
-                    shadowColor: Colors.black,
-                    elevation: 10,
-                    backgroundColor: const Color.fromRGBO(0, 0, 255, 1),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(50.0),
-                    ),
-                  ),
-                  child: const Padding(
-                    padding: EdgeInsets.fromLTRB(108.0, 15.0, 108.0, 15.0),
-                    child: Text(
-                      'Continue',
-                      style: TextStyle(
-                        fontFamily: 'BubbleboddyNeue',
-                        fontSize: 20.0,
-                        color: Color.fromRGBO(235, 235, 235, 1),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: const BorderSide(
+                          width: 1,
+                          color: Color.fromRGBO(0, 0, 255, 1),
+                        ),
+                        borderRadius: BorderRadius.circular(15.0),
                       ),
                     ),
                   ),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 70),
-                child: Text(
-                '© 2023 Cho P. Ltd, Inc. All Rights Reserved',
-                style: TextStyle(
-                  fontFamily: 'Inter',
-                  fontSize: 10.0,
-                  color: Color.fromRGBO(10, 10, 31, 1),
+                Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: ElevatedButton(
+                    onPressed: _Login,
+                    style: ElevatedButton.styleFrom(
+                      shadowColor: Colors.black,
+                      elevation: 10,
+                      backgroundColor: const Color.fromRGBO(0, 0, 255, 1),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(50.0),
+                      ),
+                    ),
+                    child: const Padding(
+                      padding: EdgeInsets.fromLTRB(105.0, 15.0, 105.0, 15.0),
+                      child: Text(
+                        'Continue',
+                        style: TextStyle(
+                          fontFamily: 'BubbleboddyNeue',
+                          fontSize: 20.0,
+                          color: Color.fromRGBO(235, 235, 235, 1),
+                        ),
+                      ),
+                    ),
+                  ),
                 ),
-                          ),
-              )
-            ],
+                Padding(
+                  padding: const EdgeInsets.only(top: 70),
+                  child: Text(
+                    '© 2023 Cho P. Ltd, Inc. All Rights Reserved',
+                    style: TextStyle(
+                      fontFamily: 'Inter',
+                      fontSize: 10.0,
+                      color: Color.fromRGBO(10, 10, 31, 1),
+                    ),
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       ),
     );
   }
 
+  void _Login() async {
+    String email = _emailController.text;
+    String password = _passwordController.text;
 
-void _Login() async {
-  String email = _emailController.text;
-  String password = _passwordController.text;
-  
-  User? user = await _auth.signInWithEmailAndPassword(email, password);
+    // Basic validation
+    if (email.isEmpty || password.isEmpty) {
+      // Show a snackbar or dialog to inform the user about the validation error
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text("Email and password are required."),
+          duration: Duration(seconds: 2),
+        ),
+      );
+      return;
+    }
 
-  // Basic validation
-  if (email.isEmpty || password.isEmpty) {
-    print("Unsuccessful Sign In");
-    return;
+    User? user = await _auth.signInWithEmailAndPassword(email, password);
+
+    if (user != null) {
+      print("User successfully Login");
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) {
+            return AdminBottomNav();
+          },
+        ),
+      );
+    } else {
+      // Show a snackbar or dialog to inform the user about the login failure
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text("Incorrect email or password. Please try again."),
+          duration: Duration(seconds: 2),
+        ),
+      );
+      print("Error Wrong Credentials");
+    }
   }
-
-  if (user != null) {
-    print("User successfully Login");
-    Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) {
-                        return AdminBottomNav();
-                      },
-                    ),
-                  );
-  } else {
-    print("Error Wrong Credentials");
-  }
-} }
+}
